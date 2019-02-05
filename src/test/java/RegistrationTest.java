31572015109fb054689c595c08914960fbf1984e
alapshov <lapshov.av@mail.ru>
2019-02-05 20:42:06 +0400

    FirstCommit

---
+++ src/test/java/RegistrationTest.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class RegistrationTest {
+    @AfterTest
+    public void setUp(){
+
+    }
+    @BeforeTest
+    public void tearDown(){
+
+    }
+    @Test
+    public void TestReg(){
+        
+    }
+}
